2377a02488ee6a54707365ba145cfcce190b48cf
Laura Mackew <lauramackew@Lauras-MacBook-Pro.local>
2022-07-27 19:24:40 -0400

    Created component structure and header.

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,10 @@
+import './header.styles.scss';
+
+const Header = () => (
+    <header>
+        <h1>Sounds Like</h1>
+        <h3>explore your muscial taste</h3>
+    </header>
+)
+
+export default Header;
