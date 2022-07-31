57a0923a500c6cea2e5fd2eaef8eb5e5a4402e0e
l-mack <lauramackew@gmail.com>
2022-07-31 18:22:32 -0400

    Setting state, in progress.

--- src/services/get-artists.js
+++ src/services/get-artists.js
@@ -6,10 +6,7 @@ const url = 'http://ws.audioscrobbler.com/2.0/'
 
 
 export async function getSimilarArtist (artistSearch) {
-        await axios.get(`${url}/?method=artist.getsimilar&artist=${artistSearch}&api_key=${apiKey}&format=json`, 
+        return await axios.get(`${url}/?method=artist.getsimilar&artist=${artistSearch}&api_key=${apiKey}&format=json`, 
         
         { params: { limit: 5 } })
-        .then(resp => {
-            console.log('response data', resp.data)
-        })    
 }
